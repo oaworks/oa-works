12ed8812cbf3c189acceda8913d3bce315b530bc
Sophy <sophy.ouch@erudit.org>
2020-12-16 17:31:23 +0000

    Move content to data files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
 module.exports = function(eleventyConfig) {
+  // Copy img/ to _site/
+  eleventyConfig.addPassthroughCopy("img");
+
+  // Copy css/ to _site/
   eleventyConfig.addPassthroughCopy('css');
+
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: true
+  });
+
   return {
     passthroughFileCopy: true
   };
