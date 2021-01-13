473cf1880ac974144413f865e2ec4f5724495ab3
Sophy <sophy.ouch@erudit.org>
2021-01-13 17:13:15 +0000

    Add animations, lazylinepainter js

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,9 @@ module.exports = function(eleventyConfig) {
   // Copy img/ to _site/
   eleventyConfig.addPassthroughCopy("img");
 
+  // Copy js/ to _site/
+  eleventyConfig.addPassthroughCopy("js");
+
   // Add CSS minifier
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
